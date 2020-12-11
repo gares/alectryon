cc80b66cdce88056d9ca8532fc80dcf3ee37dd62
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-12-11 13:23:12 -0500

    all: Move shared definitions to stripped-down version of core.py

--- alectryon/json.py
+++ alectryon/json.py
@@ -26,17 +26,17 @@ from importlib import import_module
 from itertools import zip_longest
 from os import path, makedirs, unlink
 
-from . import serapi
+from . import core
 
 TYPE_OF_ALIASES = {
-    "text": serapi.Text,
-    "hypothesis": serapi.Hypothesis,
-    "goal": serapi.Goal,
-    "message": serapi.Message,
-    "sentence": serapi.Sentence,
-    "goals": serapi.Goals,
-    "messages": serapi.Messages,
-    "rich_sentence": serapi.RichSentence,
+    "text": core.Text,
+    "hypothesis": core.Hypothesis,
+    "goal": core.Goal,
+    "message": core.Message,
+    "sentence": core.Sentence,
+    "goals": core.Goals,
+    "messages": core.Messages,
+    "rich_sentence": core.RichSentence,
 }
 
 ALIASES_OF_TYPE = {
