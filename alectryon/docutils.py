2d2620521fdf3f9be5500bbed23096a9d9ecb34f
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-12-14 21:08:36 -0500

    docutils: Add a /coq/ prefix to pygments settings in reST field lists

--- alectryon/docutils.py
+++ alectryon/docutils.py
@@ -137,7 +137,7 @@ def _alectryon_state(document):
 
 class Config:
     def __init__(self, document):
-        self.tokens = {}
+        self.tokens_by_lang = {}
         self.sertop_args = []
         # Sphinx doesn't translate ``field_list`` to ``docinfo``
         selector = lambda n: isinstance(n, (nodes.field_list, nodes.docinfo))
@@ -148,8 +148,15 @@ class Config:
 
     def parse_docinfo_field(self, node, name, body):
         if name.startswith("alectryon/pygments/"):
-            token = name[len("alectryon/pygments/"):]
-            self.tokens.setdefault(token, []).extend(body.split())
+            name = name[len("alectryon/pygments/"):]
+            if "/" not in name:
+                lang, token = "coq", name # legacy syntax doesn't have coq/
+            elif name.startswith("coq/"):
+                lang, token = "coq", name[len("coq/"):]
+            else:
+                return
+            lang_tokens = self.tokens_by_lang.setdefault(lang, {})
+            lang_tokens.setdefault(token, []).extend(body.split())
         elif name == "alectryon/serapi/args":
             import shlex
             self.sertop_args.extend(self.parse_args(shlex.split(body)))
@@ -323,7 +330,7 @@ class AlectryonPostTransform(OneTimeTransform):
 
     def _apply(self, **_kwargs):
         fmt, generator = self.init_generator()
-        with added_tokens(Config(self.document).tokens, lang="coq"):
+        with added_tokens(Config(self.document).tokens_by_lang.get("coq", {}), lang="coq"): # FIXME “coq”
             for node in self.document.traverse(alectryon_pending_io):
                 self.replace_one(node, fmt, generator)
 
